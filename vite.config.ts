eb60aaab1be8637c7a9799748e2b26e954a7a1cf
sky00069123 <sky00069123@gmail.com>
2025-09-05 22:18:21 +0100

    test website custom github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,30 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-// import { componentTagger } from "lovable-tagger";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   plugins: [
-//     react(),
-//     mode === 'development' &&
-//     componentTagger(),
-//   ].filter(Boolean),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/ken-ck-yu.github.io/', // <-- set to your repo name!
-});
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/ken-ck-yu.github.io/', // <-- set to your repo name!
+// });
